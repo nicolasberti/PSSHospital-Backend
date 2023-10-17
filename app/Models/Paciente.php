036f0103c759b6916a3588faa5d4dae502008ee0
Iara <iaravera3333@gmail.com>
2023-10-17 13:41:12 -0300

    Se comento HasApiTokens, HasFactory, Notifiable en paciente

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -4,13 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Passport\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
 
 
 class Paciente extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    //use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'pacientes';
 
