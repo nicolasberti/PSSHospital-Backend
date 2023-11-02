33cc99a169268ae9d10dfb67c88e00d1e7a9dd41
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-11-02 17:39:13 -0300

    Add DiaSemanaSeeder

--- database/migrations/2023_11_02_171522_create_dias_semana_table.php
+++ database/migrations/2023_11_02_171522_create_dias_semana_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('dias_semana', function (Blueprint $table) {
             $table->id();
-            $table->string('dia');
+            $table->string('dia')->unique();
             $table->timestamps();
         });
     }
---
+++ database/seeders/DiaSemanaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiaSemanaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('dias_semana')->insert(['dia' => 'Lunes']);
+        DB::table('dias_semana')->insert(['dia' => 'Martes']);
+        DB::table('dias_semana')->insert(['dia' => 'Miércoles']);
+        DB::table('dias_semana')->insert(['dia' => 'Jueves']);
+        DB::table('dias_semana')->insert(['dia' => 'Viernes']);
+        DB::table('dias_semana')->insert(['dia' => 'Sábado']);
+        DB::table('dias_semana')->insert(['dia' => 'Domingo']);
+    }
+}
