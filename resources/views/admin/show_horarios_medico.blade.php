96d2c69c84ea7fa1f4a487a8c70616a615310538
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-11-03 17:38:00 -0300

    Mejora GUI Consulta Horario Medico

--- resources/views/admin/show_horarios_medico.blade.php
+++ resources/views/admin/show_horarios_medico.blade.php
@@ -1,33 +1,31 @@
 @extends('template')
 
 @section('sidebar')
-        <div class="card-body">
-            <img style="width: 100px; height: 100px;" src="https://cdn-icons-png.flaticon.com/512/560/560199.png" alt="Imagen de perfil" class="img-fluid rounded-circle">
-            <p>Nombre administrador</p>
-        </div>
-        <div class="logout-button">
-            <a href="#" class="btn btn-danger">Salir</a>
-        </div>
+    <div class="card-body">
+        <img style="width: 100px; height: 100px;" src="https://cdn-icons-png.flaticon.com/512/560/560199.png" alt="Imagen de perfil" class="img-fluid rounded-circle">
+        <p>Nombre administrador</p>
+    </div>
+    <div class="logout-button">
+        <a href="#" class="btn btn-danger">Salir</a>
+    </div>
 @endsection
 
 @section('contenido')
     <h1>Horario Medico</h1>
-    <div class="card shadow">
-        <h3>Nombre</h3>
-        <h3>DNI</h3>
-        <h3>Dias</h3>
-        <h3>Horario</h3>
-        <h3>Duracion</h3>
-        <div>
-            <div class="col">
-                <form method="GET" action="{{route('admin.index')}}">
+    <div class="card shadow p-3">
+        <div class="mb-3">
+            <h3 class="d-inline-block mr-3">Nombre:</h3>
+            <h3 class="d-inline-block">DNI:</h3>
+        </div>
+        <h3 class="mb-3">Dias</h3>
+        <h3 class="mb-3">Horario</h3>
+        <h3 class="mb-3">Duracion</h3>
+        <div class="mb-3">
+            <div class="col text-center">
+                <form method="GET" action="{{ route('admin.index') }}" class="d-inline-block">
                     <button type="submit" class="btn btn-primary">Editar</button>
                 </form>
-            </div>
-        </div>
-        <div class="col">
-            <div class="col">
-                <form method="GET" action="{{route('admin.index')}}">
+                <form method="GET" action="{{ route('admin.index') }}" class="d-inline-block ml-2">
                     <button type="submit" class="btn btn-primary">Volver</button>
                 </form>
             </div>
