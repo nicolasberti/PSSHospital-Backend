ae4d667715c9a6e7c533669e03b1b1c7f1f72602
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-11-04 20:55:15 -0300

    Add Models

---
+++ app/Models/HorarioDeAtencionDiaSemana.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HorarioDeAtencionDiaSemana extends Model
+{
+    use HasFactory;
+    protected $table = 'horarios_de_atencion_dias_semana';
+}
