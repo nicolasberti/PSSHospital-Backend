1b2420426dc3c8e892e354e9954ced53e390b635
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-11-04 14:09:41 -0300

    correcciones post merge

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,6 @@ class DatabaseSeeder extends Seeder
             MedicoSeeder::class,
             SecretarioSeeder::class,
             PacienteSeeder::class,
-            DiaSemanaSeeder::class,
-            HorarioAtencionSeeder::class,
             PacienteMedicoSeeder::class,
         ]);
     }
