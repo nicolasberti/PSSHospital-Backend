a20ff90f6384c2ecf275c0edb6c879492f2b108f
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-11-06 21:38:38 -0300

    Add 10 Medicos y sus HorariosDeAtencion

--- database/seeders/HorarioDeAtencionDiaSemanaSeeder.php
+++ database/seeders/HorarioDeAtencionDiaSemanaSeeder.php
@@ -14,25 +14,32 @@ class HorarioDeAtencionDiaSemanaSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('horarios_de_atencion_dias_semana')->insert([
-            'id_horario_de_atencion' => 1,
-            'id_dias_semana' => 1
-        ]);
-        DB::table('horarios_de_atencion_dias_semana')->insert([
-            'id_horario_de_atencion' => 1,
-            'id_dias_semana' => 2
-        ]);
-        DB::table('horarios_de_atencion_dias_semana')->insert([
-            'id_horario_de_atencion' => 1,
-            'id_dias_semana' => 3
-        ]);
-        DB::table('horarios_de_atencion_dias_semana')->insert([
-            'id_horario_de_atencion' => 1,
-            'id_dias_semana' => 4
-        ]);
-        DB::table('horarios_de_atencion_dias_semana')->insert([
-            'id_horario_de_atencion' => 1,
-            'id_dias_semana' => 5
-        ]);
+        // Asociación los primeros 5 HorarioDeAtencion con los id_dias_semana 1, 3 y 5
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('horarios_de_atencion_dias_semana')->insert([
+                'id_horario_de_atencion' => $i,
+                'id_dias_semana' => 1
+            ]);
+            DB::table('horarios_de_atencion_dias_semana')->insert([
+                'id_horario_de_atencion' => $i,
+                'id_dias_semana' => 3
+            ]);
+            DB::table('horarios_de_atencion_dias_semana')->insert([
+                'id_horario_de_atencion' => $i,
+                'id_dias_semana' => 5
+            ]);
+        }
+
+        // Asocia los otros 5 HorarioDeAtencion con los id_dias_semana 2 y 4
+        for ($i = 6; $i <= 10; $i++) {
+            DB::table('horarios_de_atencion_dias_semana')->insert([
+                'id_horario_de_atencion' => $i,
+                'id_dias_semana' => 2
+            ]);
+            DB::table('horarios_de_atencion_dias_semana')->insert([
+                'id_horario_de_atencion' => $i,
+                'id_dias_semana' => 4
+            ]);
+        }
     }
 }
