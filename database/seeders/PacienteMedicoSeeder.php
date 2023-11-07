0c708b783694ce9c1be3e1e278d43303f202762f
Bruno Velazquez <42704038+BrunoVelazquez@users.noreply.github.com>
2023-11-06 21:45:44 -0300

    Fix bad Merge

--- database/seeders/PacienteMedicoSeeder.php
+++ database/seeders/PacienteMedicoSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
-<<<<<<< HEAD
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -36,72 +37,5 @@ class PacienteMedicoSeeder extends Seeder
                 'id_medico' => $medicoId,
             ]);
         }
-=======
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-
-class PacienteMedicoSeeder extends Seeder
-{
-    public function run()
-    {
-        $citas = [
-            [
-                'fecha' => '2023-11-06',
-                'horarioInicio' => '08:00:00',
-                'horarioFin' => '08:59:00',
-                'duracion' => '00:59:00',
-                'state' => 'Pendiente',
-                'diagnostico' => 'Diagnóstico del paciente 1',
-                'paciente_id' => 1, // ID de un paciente existente
-                'medico_id' => 1, // ID de un médico existente
-            ],
-            [
-                'fecha' => '2023-11-08',
-                'horarioInicio' => '08:00:00',
-                'horarioFin' => '08:59:00',
-                'duracion' => '00:59:00',
-                'state' => 'Pendiente',
-                'diagnostico' => 'Diagnóstico del paciente 1',
-                'paciente_id' => 1, // ID de un paciente existente
-                'medico_id' => 1, // ID de un médico existente
-            ],
-            [
-                'fecha' => '2023-11-05',
-                'horarioInicio' => '08:00:00',
-                'horarioFin' => '08:59:00',
-                'duracion' => '00:59:00',
-                'state' => 'Pendiente',
-                'diagnostico' => 'Diagnóstico del paciente 1',
-                'paciente_id' => 1, // ID de un paciente existente
-                'medico_id' => 1, // ID de un médico existente
-            ],
-            [
-                'fecha' => '2023-11-05',
-                'horarioInicio' => '08:00:00',
-                'horarioFin' => '08:59:00',
-                'duracion' => '00:59:00',
-                'state' => 'Realizada',
-                'diagnostico' => 'Hola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola HolaHola Hola Hola Hola',
-                'paciente_id' => 1, // ID de un paciente existente
-                'medico_id' => 1, // ID de un médico existente
-            ],
-            [
-                'fecha' => '2023-11-05',
-                'horarioInicio' => '08:00:00',
-                'horarioFin' => '08:59:00',
-                'duracion' => '00:59:00',
-                'state' => 'Realizada',
-                'diagnostico' => 'Diagnóstico del paciente 1',
-                'paciente_id' => 1, // ID de un paciente existente
-                'medico_id' => 1, // ID de un médico existente
-            ],
-
-        ];
-
-        // Insertar datos en la tabla 'paciente_medico'
-        DB::table('paciente_medico')->insert($citas);
->>>>>>> Sprint-3-paciente
     }
 }
