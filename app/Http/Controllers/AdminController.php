6e6ea44b73f8d155d77d497c743ec7c508e76e17
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-11-07 00:31:05 -0300

    Validacion editar secretario

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -133,49 +133,73 @@ class AdminController extends Controller
 
     public function edit_secretario(string $secretario){
         $secretario = Secretario::find($secretario);
-        return view('admin.edit_secretario', ['secretario' => $secretario]);
+        $provincias = Provincia::all();
+        $localidades = Localidad::all();
+        return view('admin.edit_secretario', ['secretario' => $secretario, 'provincias' => $provincias, 'localidades' => $localidades]);
     }
 
-    public function update_secretario(Request $request, string $secretarioid) {
-        $secretario = Secretario::find($secretarioid);
+    public function update_secretario(Request $request, $secretario) {
+        $validatedData = $request->validate([
+            'DNI' => 'required|numeric|unique:secretarios,DNI|digits:8',
+            'username' => 'required|string|unique:secretarios,username',
+            'password' => 'required|string',
+            'name' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|numeric',
+            'birthday' => 'required|date|before:today',
+            'address' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string',
+            'provincia' => 'required|string'
+        ], [
+            'DNI.required' => 'El campo DNI es obligatorio.',
+            'DNI.numeric' => 'El campo DNI debe ser numérico.',
+            'DNI.unique' => 'Ya existe un usuario con este DNI.',
+            'DNI.digits' => 'El campo DNI debe tener 8 dígitos.',
+            'username.required' => 'El campo Usuario es obligatorio.',
+            'username.string' => 'El campo Usuario debe ser un string.',
+            'username.unique' => 'Ya existe un usuario con este nombre de usuario.',
+            'password.required' => 'El campo Contraseña es obligatorio.',
+            'password.string' => 'El campo Contraseña debe ser un string',
+            'name.required' => 'El campo Nombre es obligatorio.',
+            'name.string' => 'El campo Nombre debe ser un string',
+            'lastname.required' => 'El campo Apellido es obligatorio.',
+            'lastname.string' => 'El campo Apellido debe ser un string',
+            'email.required' => 'El campo Email es obligatorio.',
+            'email.string' => 'El campo Email debe ser un string',
+            'email.email' => 'El campo Email debe contener el simbolo @.',
+            'phone.required' => 'El campo Celular es obligatorio.',
+            'phone.string' => 'El campo Celular debe ser un string',
+            'birthday.required' => 'El campo Fecha de Cumpleaños es obligatorio.',
+            'address.required' => 'El campo Direccion es obligatorio.',
+            'address.string' => 'El campo Direccion debe ser un string',
+            'city.required' => 'El campo Ciudad es obligatorio.',
+            'city.string' => 'El campo Ciudad debe ser un string',
+            'state.required' => 'El campo Estado es obligatorio.',
+            'state.string' => 'El campo Estado debe ser un string',
+        ]);
+        $secretarioid = $secretario;
 
-        $passwordAnterior = $secretario->password;
-        $dniAnterior = $secretario->DNI;
-        $nombreAnterior = $secretario->name;
-        $apellidoAnterior = $secretario->lastname;
-        $emailAnterior = $secretario->email;
-        $telefonoAnterior = $secretario->phone;
-        $fechaAnterior = $secretario->dateOfBirth;
-        $direccionAnterior = $secretario->adress;
-        $ciudadAnterior = $secretario->city;
-        $estadoAnterior = $secretario->state;
-
-
-
-        // Actualiza los campos solo si son diferentes a los valores anteriores
-        //falta validar bien si los valores corresponden a nombres, emails, numeros, etc.
-
-        $secretario->name = $request->filled('name') ? $request->input('name') : $nombreAnterior;
-        $secretario->email = $request->filled('email') ? $request->input('email') : $emailAnterior;
-        $secretario->phone = $request->filled('phone') ? $request->input('phone') : $telefonoAnterior;
-        $secretario->lastname = $request->filled('lastname') ? $request->input('lastname') : $apellidoAnterior;
-        $secretario->DNI = $request->filled('DNI') ? $request->input('DNI') : $dniAnterior;
-        $secretario->dateOfBirth = $request->filled('dateOfBirth') ? $request->input('dateOfBirth') : $fechaAnterior;
-        $secretario->adress = $request->filled('adress') ? $request->input('adress') : $direccionAnterior;
-        $secretario->city = $request->filled('city') ? $request->input('city') : $ciudadAnterior;
-        $secretario->state = $request->filled('state') ? $request->input('state') : $estadoAnterior;
-
-        // Actualiza la contraseña solo si se proporciona una nueva contraseña
-        if ($request->filled('password')) {
-            $secretario->password = bcrypt($request->input('password'));
-        } else {
-            $secretario->password = $passwordAnterior;
-        }
+        $secretario = Secretario::find( $secretarioid );
+        $secretario->DNI = $validatedData['DNI'];
+        $secretario->password = $validatedData['password'];
+        $secretario->name = $validatedData['name'];
+        $secretario->lastname = $validatedData['lastname'];
+        $secretario->email = $validatedData['email'];
+        $secretario->phone = $validatedData['phone'];
+        $secretario->dateOfBirth = $validatedData['birthday'];
+        $secretario->address = $validatedData['address'];
+        $secretario->ciudad = $validatedData['city'];
+        $secretario->estado = $validatedData['state'];
+        $secretario->provincia = $validatedData['provincia'];
 
         $secretario->save();
 
         $secretarios = Secretario::all();
-        return redirect()->route('admin.show_secretarios', ['secretarios' => $secretarios]);
+        return redirect()->route('admin.show_secretarios', ['secretarios' => $secretarios])
+        ->with('success', 'El secretario se ha registrado con éxito')
+        ->with('alert', 'success');
     }
 
     public function show_baja_secretarios(){
